9eafb3ec96eaaa754cf83870ba7a0db6a674e69b
MIClavelTe <miclavelte@gmail.com>
2018-11-21 16:59:55 -0800

    Edit Elements

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const title = 'Using REACT';
-const description = 'First time using REACT';
-var titleId = 'main-title'
+const titleId = 'main-title';
+const descId = 'main-desc';
+const name = 'Maria';
+// A Comment in JSX looks like this:
+// {/* TEXT */}
 
 const header = ( 
     <header>
-        <h1 id={titleId}>{ title }</h1>
-        <p>{ description }</p>
+        <h1 id={titleId}>{ name }'s REACT</h1>
+        <p id={descId}>{ name }'s first time using REACT</p>
     </header>
 );
 
