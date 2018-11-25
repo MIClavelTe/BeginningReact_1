e9590934e3977aa3306b493bd35a4a411d4bcea9
MIClavelTe <miclavelte@gmail.com>
2018-11-24 18:17:48 -0800

    Edit elements

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const header = ( 
-    <header>
-        <h1 id={titleId}>{ name }'s REACT</h1>
-        <p id={descId}>{ name }'s first time using REACT</p>
-    </header>
-);
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 3/5</span>
+        </header>
+    );
+};
+
+function Player() {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Maria
+            </span>
+            <Counter />
+        </div>
+    );
+};
+
+function Counter() {
+    return(
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">150</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+};
+
+function App() {
+    return (
+        <div className='scoreboard'>
+            <Header />
+            <Player />
+        </div>
+    );
+};
 
 ReactDOM.render(
-    header, document.getElementById('root')
+    <App />, document.getElementById('root')
 );
